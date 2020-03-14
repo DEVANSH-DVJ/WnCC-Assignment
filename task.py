c000b58fda7cdbe71fbca4c90b24c45604219d5f
devansh-dvj <devansh.dvj@gmail.com>
2020-03-14 22:07:28 +0530

    Added another function which is fool-proof but slower, Python.

--- task.py
+++ task.py
@@ -1,5 +1,30 @@
+def extractTimeExtensive(line, list):
+    minutes = 0
+    milliseconds = 0
+
+    i = 3
+    min = True
+    while True:
+        i += 1
+        if line[i] == 's':
+            break
+        elif line[i] == '.' or line[i] == ' ':
+            continue
+        elif line[i] == 'm':
+            min = False
+        elif min:
+            minutes *= 10
+            minutes += float(line[i])
+        else:
+            milliseconds *= 10
+            milliseconds += float(line[i])
+
+    seconds = (minutes * 60) + (milliseconds / 1000)
+    list.append(round(seconds, 3))
+    # Due to inaccuracy of float, trailing decimal places might not be zero.
+
+# Trailing spaces would cause wrong problems, but it is faster.
 def extractTime(line, list):
-    # print('Hi')
     seconds = 0
     factor = 0.001
     i = -2
@@ -10,7 +35,7 @@ def extractTime(line, list):
         elif line[i] == '.':
             continue
         else:
-            seconds += float(line[i])*factor
+            seconds += float(line[i]) * factor
             factor *= 10
 
     factor = 60
@@ -19,11 +44,10 @@ def extractTime(line, list):
         if line[i] == ' ':
             break
         else:
-            seconds += float(line[i])*factor
+            seconds += float(line[i]) * factor
             factor *= 10
-    list.append(round(seconds,3))
+    list.append(round(seconds, 3))
     # Due to inaccuracy of float, sometimes 3e-17 was added.
-
 
 def main():
     print('Hello')
@@ -34,11 +58,11 @@ def main():
     with open('timestat.txt', 'r') as timestat_data:
         for line in timestat_data:
             if line[0] == 'r':
-                extractTime(line, real)
+                extractTimeExtensive(line, real)
             elif line[0] == 'u':
-                extractTime(line, user)
+                extractTimeExtensive(line, user)
             elif line[0] == 's':
-                extractTime(line, sys)
+                extractTimeExtensive(line, sys)
     print(real)
     print(sys)
     print(user)
